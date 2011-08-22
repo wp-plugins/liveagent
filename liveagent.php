f944fa78ec52d412012c56636bb940137d261161
jurajsim <jurajsim@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-08-22 10:20:30 +0000

    ver. 1.2.2 commit - bugfixes
    git-svn-id: https://plugins.svn.wordpress.org/liveagent/tags/1.2.2@427042 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- liveagent.php
+++ liveagent.php
@@ -4,14 +4,14 @@
  Plugin URI: http://www.qualityunit.com/liveagent
  Description: Plugin that enable integration with Live Agent
  Author: QualityUnit
- Version: 1.1.5
+ Version: 1.2.2
  Author URI: http://www.qualityunit.com
  License: GPL2
  */
 
-define('LIVEAGENT_PLUGIN_VERSION', '1.1.5');
+define('LIVEAGENT_PLUGIN_VERSION', '1.2.1');
 define('LIVEAGENT_PLUGIN_NAME', 'liveagent');
-include WP_PLUGIN_DIR . '/' . LIVEAGENT_PLUGIN_NAME . '/Config.class.php';
+require_once WP_PLUGIN_DIR . '/' . LIVEAGENT_PLUGIN_NAME . '/Config.class.php';
 
 $loadErrorMessage = null;
 
@@ -46,30 +46,40 @@ if (!class_exists('liveagent')) {
          */
         private $auth;
 
+        /**
+         *  @var liveagent_AjaxHandler
+         */
+        private $ajaxHandler;
+
         public function __construct() {
             $this->buttonHelper = new liveagent_helper_Buttons();
             $this->settings = new liveagent_Settings();
             $this->initPlugin();
+
+            /*$session = new Gpf_Api_Session('http://www.qualityunit.com/affiliate/scripts/track.php');
+             $tracker = new Pap_Api_Tracker();
+             $tracker->track();*/
         }
 
         private function getLaIconURL() {
             return $this->getImgUrl() . 'menu-icon.png';
         }
 
-
         private function initPlugin() {
             $this->auth = new liveagent_Auth();
+            $this->ajaxHandler = new liveagent_AjaxHandler($this->settings, $this->auth);
+            add_filter ('init', array($this->ajaxHandler, 'handle'), 99);
             add_action('admin_init', array($this->settings, 'initSettings'));
             add_action('admin_menu', array($this, 'addPrimaryConfigMenu'));
             add_filter ('wp_head', array($this, 'initHeader'), 99);
             add_filter ('admin_head', array($this, 'initAdminHeader'), 99);
-            if (!$this->settings->settingsDefinedForConnection()) {
+            if (!$this->includeJavascript('liveagent-jquery', 'jQuery.js')) {
                 return;
             }
-            if (function_exists('wp_enqueue_script')) {
-                wp_enqueue_script('liveagent-main', $this->getJsUrl() . 'main.js', array(), LIVEAGENT_PLUGIN_VERSION);
-            } else {
-                $this->showAdminError(__('Live Agent plugin error: Unable to load required javascript files! Wordpress function wp_enqueue_script is missing.', LIVEAGENT_PLUGIN_NAME));
+            if (!$this->includeJavascript('liveagent-main', 'main.js')) {
+                return;
+            }
+            if (!$this->settings->settingsDefinedForConnection()) {
                 return;
             }
             add_filter ('wp_footer', array($this, 'initFooter'), 99);
@@ -80,6 +90,16 @@ if (!class_exists('liveagent')) {
             }
             $widgetIntegrator = new liveagent_WidgetIntegrator($this->settings, $this->buttonHelper);
             $widgetIntegrator->initWidgets();
+        }
+
+        private function includeJavascript($handle, $jsName) {
+            if (function_exists('wp_enqueue_script')) {
+                wp_enqueue_script($handle, $this->getJsUrl() . $jsName, array(), LIVEAGENT_PLUGIN_VERSION);
+                return true;
+            } else {
+                $this->showAdminError(__('Live Agent plugin error: Unable to load required javascript files! Wordpress function wp_enqueue_script is missing.', LIVEAGENT_PLUGIN_NAME));
+                return false;
+            }
         }
 
         private function initCommonHeaders() {
@@ -119,11 +139,19 @@ if (!class_exists('liveagent')) {
 
         public function addPrimaryConfigMenu() {
             add_menu_page(__('Live Agent', LIVEAGENT_PLUGIN_NAME), __('Live Agent',LIVEAGENT_PLUGIN_NAME), 'manage_options', 'la-top-level-options-handle', array($this, 'printGeneralConfigPage'), $this->getLaIconURL());
+            if (!strlen(trim($this->settings->getLiveAgentUrl())) || !strlen(trim($this->settings->getOwnerEmail()))) {
+                return;                
+            }
             add_submenu_page('la-top-level-options-handle', __('Buttons',LIVEAGENT_PLUGIN_NAME), __('Buttons',LIVEAGENT_PLUGIN_NAME), 'manage_options', 'buttons-config-page', array($this, 'printButtonsConfigPage'));
         }
 
         public function printGeneralConfigPage() {
-            $form = new liveagent_Form_Settings_Account($this->settings, $this->auth);
+            if ($this->getAccountStatus() == self::ACCOUNT_STATUS_NOTSET) {
+                $form = new liveagent_Form_Settings_Signup($this->settings, $this->auth);
+
+            } else {
+                $form = new liveagent_Form_Settings_Account($this->settings, $this->auth);
+            }
             $form->render();
         }
 
