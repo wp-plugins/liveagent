a48da3b01e55250f004cec0b1dd408a0187fa53f
jurajsim <jurajsim@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-01 08:07:54 +0000

    commit 1.2.10
    git-svn-id: https://plugins.svn.wordpress.org/liveagent/tags/1.2.10@498393 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- helper/Buttons.class.php
+++ helper/Buttons.class.php
@@ -22,6 +22,27 @@ class liveagent_helper_Buttons extends liveagent_helper_Grid {
             default: return 'Unknown';
         }
     }
+    
+    private function enableButton($buttonId) {
+        $settings = new liveagent_Settings();
+        if ($settings->buttonIsEnabled($buttonId)) {
+            return;    
+        }
+        $value = get_option(liveagent_Settings::BUTTONS_CONFIGURATION_SETTING_NAME);
+        if ($value == '' || $value === null) {
+            $value = array($buttonId => 'true');
+        }
+        if (!array_key_exists($buttonId, $value)) {
+            $value[$buttonId] = 'true';
+        }
+        update_option(liveagent_Settings::BUTTONS_CONFIGURATION_SETTING_NAME, $value);
+    }
+    
+    public function enableDefaultButton(){
+        //b2222222 was id of old default button. from v. 2.0.53.2+ it will onlu button1 
+        $this->enableButton('b2222222');
+        $this->enableButton('button1');
+    }
 
     public function isSomeButtonEnabled() {
         $buttons = get_option(liveagent_Settings::BUTTONS_CONFIGURATION_SETTING_NAME);
@@ -65,7 +86,7 @@ class liveagent_helper_Buttons extends liveagent_helper_Grid {
         return $this->getField($buttonId, 'offlinecode');
     }
 
-    public function getIntegrationCode($buttonId) {
+    public function getIntegrationCode($buttonId) {        
         return '<img src="'.$this->getRemotePixUrl().'" onLoad="LiveAgentTracker.createButton(\''.$buttonId.'\', this);"/>' . "\n";
     }
 
